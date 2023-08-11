89634a48e5f81ea2075fffcdcf046901cf9ba402
sudheer-kumar-ch <sudheerCsk77@gmail.com>
2023-08-11 07:08:11 +0000

    superOverLeague

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bgContainer">
+    <div>
+      <h1 className="heading">Super Over League</h1>
+      <div>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        ></img>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        ></img>
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
